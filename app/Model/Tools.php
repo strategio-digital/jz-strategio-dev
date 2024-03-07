d517dc5b1ce820a0d7a65701ff6a60de2d5fb78b
Jiří Zapletal <jz@strategio.dev>
2024-03-06 20:23:00 -0700

    Improved UX and typo

--- app/Model/Tools.php
+++ app/Model/Tools.php
@@ -17,33 +17,33 @@ class Tools
         return [
             [
                 'title' => 'Megio panel',
-                'description' => 'Nástroj, ve kterém Vám doslova naklikám profesionální administraci webu či aplikace.',
+                'description' => 'Nástroj, díky kterému vám ušetřím více než 70 % za vývoj aplikace, REST API nebo webu na míru.',
                 'link' => '/blog/megio'
             ],
             [
                 'title' => 'Contentio web',
-                'description' => 'Nástroj, díky kterému Vám vytvořím kompletní webové stránky v řádu hodin.',
+                'description' => 'Nástroj, ve kterém vám na jedno kliknutí založím celou administraci a REST API pro klasický web.',
                 'link' => 'https://contentio.app'
             ],
             [
                 'title' => 'Contactio mail',
-                'description' => 'Nástroj pro hromadné posílání e-mailů, pomocí kterého o Vás dám světu vědět.',
+                'description' => 'Nástroj pro automatické rozesílání transakčních a marketingových e-mailů (REST API se plánuje).',
                 'link' => 'https://contactio.app'
             ],
             [
                 'title' => 'Invoice GUN',
-                'description' => 'Nástroj pro Vaší kompletní fakturaci - hezké faktury s QR kódem a napojením na banku.',
+                'description' => 'Nástroj pro evidenci faktur s platebním QR kódem a možností ovládání přes REST API.',
                 'link' => '/blog/nastroj-na-faktury-zdarma'
             ],
             [
-                'title' => 'Webis editor',
-                'description' => 'Nástroj pro rychlou tvorbu Vašeho webu napojeného na Megio admin panel.',
-                'link' => null
+                'title' => 'Cookie Log',
+                'description' => 'Nástroj pro ukládání souhlasů s cookies, včetně změn, v souladu se zákonem o GDPR.',
+                'link' => 'https://documenter.getpostman.com/view/14885541/2s935poNYQ'
             ],
             [
-                'title' => 'Cookie Log',
-                'description' => 'Nástroj pro ukládání souhlasů s cookies (vč. změn) v souladu se zákonem o GDPR.',
-                'link' => 'https://documenter.getpostman.com/view/14885541/2s935poNYQ'
+                'title' => 'Webis editor',
+                'description' => 'Nástroj ve kterém si budete sami schopni vytvořit web bez znalosti programování.',
+                'link' => null
             ],
         ];
     }
