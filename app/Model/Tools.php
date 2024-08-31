59c7de0dfed303adbeef846ce07d53e18f8d1f9c
Jiří Zapletal <jz@strategio.dev>
2024-08-31 03:42:34 +0200

    Improved tools

--- app/Model/Tools.php
+++ app/Model/Tools.php
@@ -17,33 +17,39 @@ class Tools
         return [
             [
                 'title' => 'Megio panel',
-                'description' => 'Nástroj, díky kterému vám ušetřím více než 70 % za vývoj aplikace, REST API nebo webu na míru.',
-                'link' => '/blog/megio'
+                'description' => 'Nástroj, díky kterému ušetříte více než 70 % za vývoj aplikace, administrace, REST API či systému na míru.',
+                'link' => '/blog/megio',
+                'button' => 'Přečíst článek (proč a jak)'
             ],
             [
-                'title' => 'Contentio web',
-                'description' => 'Nástroj, ve kterém vám na jedno kliknutí založím celou administraci a REST API pro klasický web.',
-                'link' => 'https://contentio.app'
+                'title' => 'Webis editor',
+                'description' => 'Webový editor s umělou inteligencí, ve kterém si vytvoří web i běžní uživatelé - bez znalosti kódu a programování.',
+                'link' => 'https://webis.app',
+                'button' => 'Vytvořit web ZDARMA'
             ],
             [
                 'title' => 'Contactio mail',
                 'description' => 'Nástroj pro automatické rozesílání transakčních a marketingových e-mailů (REST API se plánuje).',
-                'link' => 'https://contactio.app'
+                'link' => 'https://contactio.app',
+                'button' => 'Založit e-mailovou kampaň'
+            ],
+            [
+                'title' => 'Contentio web',
+                'description' => 'Nástroj, ve kterém vám na jedno kliknutí založím celou administraci a REST API pro klasický web.',
+                'link' => 'https://contentio.app',
+                'button' => 'Přejít na web projektu'
             ],
             [
                 'title' => 'Invoice GUN',
                 'description' => 'Nástroj pro evidenci faktur s platebním QR kódem a možností ovládání přes REST API.',
-                'link' => '/blog/nastroj-na-faktury-zdarma'
+                'link' => '/blog/nastroj-na-faktury-zdarma',
+                'button' => 'Přečíst článek (co to umí)'
             ],
             [
                 'title' => 'Cookie Log',
                 'description' => 'Nástroj pro ukládání souhlasů s cookies, včetně změn, v souladu se zákonem o GDPR.',
-                'link' => 'https://documenter.getpostman.com/view/14885541/2s935poNYQ'
-            ],
-            [
-                'title' => 'Webis editor',
-                'description' => 'Nástroj ve kterém si budete sami schopni vytvořit web bez znalosti programování.',
-                'link' => null
+                'link' => 'https://documenter.getpostman.com/view/14885541/2s935poNYQ',
+                'button' => 'REST API Dokumentace'
             ],
         ];
     }
